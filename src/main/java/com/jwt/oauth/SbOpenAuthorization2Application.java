ab6c634b52b71acf8b514535554970cb33c2b9c6
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-25 14:07:24 +0530

    initial commit, OAuth2.0 in Spring Boot application

---
+++ src/main/java/com/jwt/oauth/SbOpenAuthorization2Application.java
@@ -0,0 +1,14 @@
+package com.jwt.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOpenAuthorization2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbOpenAuthorization2Application.class, args);
+		System.out.println("\n*****OPEN\n  AUTHENTICATION\n  2.0*******\n");
+	}
+
+}
