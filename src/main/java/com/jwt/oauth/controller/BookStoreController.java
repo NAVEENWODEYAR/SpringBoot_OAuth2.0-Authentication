395be047c87f28b49b1a5d07de14aab69371393e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-02 23:38:00 +0530

    404 while accessing the endPoint

--- src/main/java/com/jwt/oauth/controller/BookStoreController.java
+++ src/main/java/com/jwt/oauth/controller/BookStoreController.java
@@ -1,5 +1,10 @@
 package com.jwt.oauth.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/bookStore")
 public class BookStoreController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookStoreController.class);
+
+    @GetMapping
+    public ResponseEntity<?> greet(){
+        LOGGER.info("Accessed test endPoint,");
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Content-Type", "application/String")
+                .body("Welcome to OpenAuthentication 2.0, using GitHub,,");
+    }
 }
