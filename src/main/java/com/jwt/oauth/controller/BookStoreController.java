1d63a102503115a8632a1c2fbc9463edc9cd3771
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2025-08-03 22:19:32 +0530

    Update BookStoreController.java

--- src/main/java/com/jwt/oauth/controller/BookStoreController.java
+++ src/main/java/com/jwt/oauth/controller/BookStoreController.java
@@ -20,7 +20,7 @@ public class BookStoreController {
 
     @GetMapping
     public ResponseEntity<?> greet(){
-        LOGGER.info("Accessed test endPoint,");
+        LOGGER.info("Accessed/Inside test endPoint,");
         return ResponseEntity.status(HttpStatus.FOUND)
                 .header("Content-Type", "application/String")
                 .body("Welcome to OpenAuthentication 2.0, using GitHub,,");
